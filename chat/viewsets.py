f80f3312303880adb5b7fce49e54f384845e2e49
RichaChauhan1989 <richasain1989@gmail.com>
2024-06-13 10:12:43 +1200

    commit message module

--- chat/viewsets.py
+++ chat/viewsets.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from chat.models import ChatRoom
-from chat.Serializers import ChatRoomSerializer
+from chat.models import ChatRoom, Message
+from chat.Serializers import ChatRoomSerializer, MessageSerializer
+
 
 
 class ChatRoomViewSet(viewsets.ModelViewSet):
     queryset = ChatRoom.objects.all()
     serializer_class = ChatRoomSerializer
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
