00b05e4d8b045dd78f1981cedf807d5ef77672dc
RichaChauhan1989 <richasain1989@gmail.com>
2024-06-24 17:06:01 +1200

    update Django test code

--- chat/tests.py
+++ chat/tests.py
@@ -1,55 +1,68 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
+from rest_framework import status
 from .models import ChatRoom
-from .serializers import ChatRoomSerializer
+from .Serializers import ChatRoomSerializer
+from django.urls import reverse
+
 
 class ChatRoomViewSetTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.room1 = ChatRoom.objects.create(name='Room 1')
         self.room2 = ChatRoom.objects.create(name='Room 2')
+        self.valid_payload = {
+            'name': 'New Room'
+        }
+        self.invalid_payload = {
+            'name': ''
+        }
 
     def test_list_chatrooms(self):
-        # Test listing all chatrooms using GET request
-        response = self.client.get('/api/chatrooms/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)  # Assuming there are 2 chatrooms in the database
+        response = self.client.get(reverse('chatroom-list'))
+        chatrooms = ChatRoom.objects.all()
+        serializer = ChatRoomSerializer(chatrooms, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
 
     def test_retrieve_chatroom(self):
-        # Test retrieving a single chatroom using GET request
-        url = '/api/chatrooms/{}/'.format(self.room1.id)
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data['name'], 'Room 1')
+        response = self.client.get(reverse('chatroom-detail', kwargs={'pk': self.room1.pk}))
+        chatroom = ChatRoom.objects.get(pk=self.room1.pk)
+        serializer = ChatRoomSerializer(chatroom)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
 
     def test_create_chatroom(self):
-        # Test creating a new chatroom using POST request
-        data = {'name': 'New Room'}
-        response = self.client.post('/api/chatrooms/', data)
-        self.assertEqual(response.status_code, 201)  # 201 Created
-        self.assertEqual(ChatRoom.objects.count(), 3)  # Check if a new chatroom was created
+        response = self.client.post(reverse('chatroom-list'), data=self.valid_payload)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ChatRoom.objects.count(), 3)
+
+    def test_create_invalid_chatroom(self):
+        response = self.client.post(reverse('chatroom-list'), data=self.invalid_payload)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(ChatRoom.objects.count(), 2)
 
     def test_update_chatroom(self):
-        # Test updating an existing chatroom using PUT request
-        data = {'name': 'Updated Room Name'}
-        url = '/api/chatrooms/{}/'.format(self.room1.id)
-        response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.put(
+            reverse('chatroom-detail', kwargs={'pk': self.room1.pk}),
+            data={'name': 'Updated Room Name'},
+            format='json'
+        )
         self.room1.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.room1.name, 'Updated Room Name')
 
     def test_partial_update_chatroom(self):
-        # Test partially updating an existing chatroom using PATCH request
-        data = {'name': 'Partial Update'}
-        url = '/api/chatrooms/{}/'.format(self.room1.id)
-        response = self.client.patch(url, data, format='json')
-        self.assertEqual(response.status_code, 200)
+        response = self.client.patch(
+            reverse('chatroom-detail', kwargs={'pk': self.room1.pk}),
+            data={'name': 'Partial Update'},
+            format='json'
+        )
         self.room1.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(self.room1.name, 'Partial Update')
 
     def test_delete_chatroom(self):
-        # Test deleting an existing chatroom using DELETE request
-        url = '/api/chatrooms/{}/'.format(self.room1.id)
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, 204)  # 204 No Content
-        self.assertFalse(ChatRoom.objects.filter(id=self.room1.id).exists())
+        response = self.client.delete(reverse('chatroom-detail', kwargs={'pk': self.room1.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(ChatRoom.objects.filter(pk=self.room1.pk).exists())
