6a25c696e0d5eb410ce054e7eb5b23beb0527ba9
RichaChauhan1989 <richasain1989@gmail.com>
2024-07-04 13:39:15 +1200

    add cross origin again

--- message/settings.py
+++ message/settings.py
@@ -39,10 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'chat',
-    'corsheaders'
+    'corsheaders',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE =[
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,11 +138,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+
 CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
+
     'http://localhost:3000',
+
 ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+
 CORS_ALLOWED_ORIGIN_REGEXES = [
+
     'http://localhost:3000',
+
 ]
+# CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ] # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     'http://localhost:3000',
+# ]
