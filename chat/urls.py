c94416abef963ff2b462ae892922a22b84088afd
RichaChauhan1989 <richasain1989@gmail.com>
2024-07-08 15:26:54 +1200

    sum number

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+
+from chat.views import sumNumbersView
 from chat.viewsets import ChatRoomViewSet, MessageViewSet
 
 router = DefaultRouter()
 router.register('chatroom', ChatRoomViewSet, basename='chatroom')
 router.register('Message', MessageViewSet, basename='Message')
 urlpatterns = router.urls
+urlpatterns += [
+    path('sum_numbers/', sumNumbersView, name='sum_numbers'),
+]
